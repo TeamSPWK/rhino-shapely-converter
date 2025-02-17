1324b0a6f0dc7e3ca2846bb88bdcd0cbe3aa52a0
김 재웅 jaeung <jaeung@spacewalk.tech>
2025-02-17 13:02:04 +0900

    Update

--- tests/test_converter_from_rhino_to_shapely.py
+++ tests/test_converter_from_rhino_to_shapely.py
@@ -7,14 +7,24 @@ from src.rhino_shapely_converter.converter import rhino_geometry_to_shapely_geom
 
 @pytest.mark.parametrize("x, y, z", [(1, 2, 3), (4, 5, 6), (7, 8, 9)])
 def test_rhino_point_to_shapely_point(x, y, z):
+    # Create rhino point
     rhino_point = r3d.Point3d(x, y, z)
+
+    # Convert to shapely
     shapely_point = rhino_geometry_to_shapely_geometry(rhino_point)
+
+    # Check if those are the same
     assert shapely_point == sgeom.Point(x, y, z)
 
 def test_rhino_point_cloud_to_shapely_point_cloud():
+    # Create rhino point cloud
     rhino_point_cloud = r3d.PointCloud()
     rhino_point_cloud.Add(r3d.Point3d(1, 2, 3))
     rhino_point_cloud.Add(r3d.Point3d(4, 5, 6))
     rhino_point_cloud.Add(r3d.Point3d(7, 8, 9))
+
+    # Convert to shapely
     shapely_point_cloud = rhino_geometry_to_shapely_geometry(rhino_point_cloud)
+
+    # Check if those are the same
     assert shapely_point_cloud == sgeom.MultiPoint([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
